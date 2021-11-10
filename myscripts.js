d3c03d9882acd688986cc0c63c2aa303fb498a05
2-js <80288048+2-js@users.noreply.github.com>
2021-11-10 13:12:09 -0600

    Update myscripts.js

--- myscripts.js
+++ myscripts.js
@@ -1,1 +1,28 @@
-
+  function help() {
+  var srcT = document.getElementById('iframe').src
+  window.open(srcT)
+  }
+  function boss() {
+  document.getElementById('iframe').src = "https://www.mathplayground.com/drift-boss/index.html"
+  }
+  function germ() {
+  document.getElementById('iframe').src = "https://germs.io"
+  }
+  function kirka() {
+  document.getElementById('iframe').src = "https://kirka.io"
+  }
+  function retro() {
+  document.getElementById('iframe').src = "https://debbiewatermanphd.com/uploads/5/5/6/7/5567194/custom_themes/230188292910318641/files/rbl.html"
+  }
+  function fulls() {
+  document.getElementById('iframe').height = "680px"
+  }
+  function drifth() {
+  document.getElementById('iframe').src = "https://driftboss.io/drift-hunters.embed"
+  }
+  function slope() {
+  document.getElementById('iframe').src = "https://slopeunblocked.online/slope-unblocked.embed"
+  }
+  function fullsX() {
+  document.getElementById('iframe').height = "535px"
+  }
