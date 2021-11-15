d47a3ecf5a7b5127ace3214a80c55d68cd75a507
2-js <80288048+2-js@users.noreply.github.com>
2021-11-15 13:42:18 -0600

    Update myscripts.js

--- myscripts.js
+++ myscripts.js
@@ -1,14 +1,35 @@
-  function help() {
+function help() {
   var srcT = document.getElementById('iframe').src
   window.open(srcT)
-  }
+}
 function ball() {
   document.getElementById('iframe').src = "https://s3.amazonaws.com/media.safekidgames.com/lets-park/index.html"
 }
-function df() { 
- document.getElementById('iframe').src = "https://cookieduck.com/games/mclocal/index.html" 
+function boss() {
+  document.getElementById('iframe').src = "https://www.mathplayground.com/drift-boss/index.html"
 }
-  function hub() {
+function germ() {
+  document.getElementById('iframe').src = "https://germs.io"
+}
+function kirka() {
+  document.getElementById('iframe').src = "https://kirka.io"
+}
+function retro() {
+  document.getElementById('iframe').src = "https://debbiewatermanphd.com/uploads/5/5/6/7/5567194/custom_themes/230188292910318641/files/rbl.html"
+}
+function fulls() {
+  document.getElementById('iframe').height = "680px"
+}
+function drifth() {
+  document.getElementById('iframe').src = "https://driftboss.io/drift-hunters.embed"
+}
+function slope() {
+  document.getElementById('iframe').src = "https://slopeunblocked.online/slope-unblocked.embed"
+}
+function fullsX() {
+  document.getElementById('iframe').height = "535px"
+}
+function hub() {
   window.open("https://www.google.com/search?q=johnny sins big fat gurthy saber")
   window.open("https://www.google.com/search?q=is it possible for a student to date a teacher")
   window.open("https://www.google.com/search?q=why are white guys so hot")
@@ -21,28 +42,12 @@ function df() {
   window.open("https://www.google.com/search?q=black circumsitions")
   window.open("https://www.google.com/search?q=hot sexy black guys working out")
   window.open("https://www.google.com/search?q=how to clean out your asshole")
-  }
-  function boss() {
-  document.getElementById('iframe').src = "https://www.mathplayground.com/drift-boss/index.html"
-  }
-  function germ() {
-  document.getElementById('iframe').src = "https://germs.io"
-  }
-  function kirka() {
-  document.getElementById('iframe').src = "https://kirka.io"
-  }
-  function retro() {
-  document.getElementById('iframe').src = "https://debbiewatermanphd.com/uploads/5/5/6/7/5567194/custom_themes/230188292910318641/files/rbl.html"
-  }
-  function fulls() {
-  document.getElementById('iframe').height = "680px"
-  }
-  function drifth() {
-  document.getElementById('iframe').src = "https://driftboss.io/drift-hunters.embed"
-  }
-  function slope() {
-  document.getElementById('iframe').src = "https://slopeunblocked.online/slope-unblocked.embed"
-  }
-  function fullsX() {
-  document.getElementById('iframe').height = "535px"
-  }
+  window.open("https://www.google.com/search?q=jewish kid with hot ribs")
+  window.open("https://www.google.com/search?q=kyle writtenhouse sexy pictures posed")
+  window.open("https://www.google.com/search?q=how to hyjack a school bus")
+  window.open("https://www.google.com/search?q=how to know if im pregnant")
+  window.open("https://www.google.com/search?q=how to know if i have a std from a boy")
+  window.open("https://www.google.com/search?q=lightning mcqueen balls")
+  window.open("https://www.google.com/search?q=orphan kids fighting over food")
+  window.open("https://www.google.com/search?q=c & b torture")
+}
