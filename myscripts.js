f185b7a1d32dd60d7e08fee33c0618f9dfb0576b
2-js <80288048+2-js@users.noreply.github.com>
2021-11-17 07:50:15 -0600

    Update myscripts.js

--- myscripts.js
+++ myscripts.js
@@ -29,31 +29,3 @@ function slope() {
 function fullsX() {
   document.getElementById('iframe').height = "535px"
 }
-function hub() {
-  window.open("https://www.google.com/search?q=johnny sins gurthy saber")
-  window.open("https://www.google.com/search?q=is it possible for a student to date a teacher")
-  window.open("https://www.google.com/search?q=why are white guys so hot")
-  window.open("https://www.google.com/search?q=white guy pant buldges")
-  window.open("https://www.google.com/search?q=white guys with nice and hot oiled 6 packs")
-  window.open("https://www.google.com/search?q=video of a teacher farting")
-  window.open("https://www.google.com/search?q=are black guys hot")
-  window.open("https://www.google.com/search?q=black little boys")
-  window.open("https://www.google.com/search?q=black men")
-  window.open("https://www.google.com/search?q=black circumsitions")
-  window.open("https://www.google.com/search?q=hot sexy black guys working out")
-  window.open("https://www.google.com/search?q=africans killing for water")
-  window.open("https://www.google.com/search?q=jewish kid with ribs and oiled six pack")
-  window.open("https://www.google.com/search?q=kyle writtenhouse sexy pictures posed")
-  window.open("https://www.google.com/search?q=how to hyjack a school bus")
-  window.open("https://www.google.com/search?q=how to know if im pregnant")
-  window.open("https://www.google.com/search?q=how to know if i have a std from a boy")
-  window.open("https://www.google.com/search?q=lightning mcqueen balls")
-  window.open("https://www.google.com/search?q=orphan kids fighting over food")
-  window.open("https://www.google.com/search?q=mater bater")
-  window.open("https://www.google.com/search?q=is it weird if i taste my own pee")
-  window.open("https://www.google.com/search?q=how to not poo poo your bed at night")
-  window.open("https://www.youtube.com/watch?v=9o3f8WgE1Tk&ab_channel=SoundsFX")
-  window.open("https://www.google.com/search?q=omish woman bussin it down")
-  window.open("https://www.google.com/search?q=jewish people forced to eat pork")
-  window.open("https://www.google.com/search?q=muslim girls turbine being ripped off")
-}
