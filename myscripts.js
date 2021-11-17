e2934612414636eb66d3dec915dddcd6d3014a13
2-js <80288048+2-js@users.noreply.github.com>
2021-11-17 13:38:09 -0600

    Update myscripts.js

--- myscripts.js
+++ myscripts.js
@@ -1,51 +1,21 @@
-function help() {
-  var srcT = document.getElementById('iframe').src
-  window.open(srcT)
-}
-function ball() {
-  document.getElementById('iframe').src = "https://s3.amazonaws.com/media.safekidgames.com/lets-park/index.html"
-}
-function boss() {
-  document.getElementById('iframe').src = "https://www.mathplayground.com/drift-boss/index.html"
-}
-function germ() {
-  document.getElementById('iframe').src = "https://germs.io"
-}
-function kirka() {
-  document.getElementById('iframe').src = "https://kirka.io"
-}
-function retro() {
-  document.getElementById('iframe').src = "https://debbiewatermanphd.com/uploads/5/5/6/7/5567194/custom_themes/230188292910318641/files/rbl.html"
-}
-function fulls() {
-  document.getElementById('iframe').height = "680px"
-}
-function drifth() {
-  document.getElementById('iframe').src = "https://driftboss.io/drift-hunters.embed"
-}
-function slope() {
-  document.getElementById('iframe').src = "https://slopeunblocked.online/slope-unblocked.embed"
-}
-function fullsX() {
-  document.getElementById('iframe').height = "535px"
-}
 function fullScreenRequest(id) {
-                if (document.getElementById(id).requestFullscreen) {
-                    document.getElementById(id).requestFullscreen();
-                } else if (document.getElementById(id).mozRequestFullScreen) {
-                    document.getElementById(id).mozRequestFullScreen();
-                } else if (document.getElementById(id).webkitRequestFullscreen) {
-                    document.getElementById(id).webkitRequestFullscreen();
-                } else if (document.getElementById(id).msRequestFullscreen) {
-                    document.getElementById(id).msRequestFullscreen();
-                }
-            }
-            function closeFullscreen() {
-                if (document.exitFullscreen) {
-                    document.exitFullscreen();
-                } else if (document.webkitExitFullscreen) {
-                    document.webkitExitFullscreen();
-                } else if (document.msExitFullscreen) {
-                    document.msExitFullscreen();
-                }
-            }
+  if (document.getElementById(id).requestFullscreen) {
+    document.getElementById(id).requestFullscreen();
+} else if (document.getElementById(id).mozRequestFullScreen) {
+    document.getElementById(id).mozRequestFullScreen();
+} else if (document.getElementById(id).webkitRequestFullscreen) {
+document.getElementById(id).webkitRequestFullscreen();
+} else if (document.getElementById(id).msRequestFullscreen) {
+document.getElementById(id).msRequestFullscreen();
+ }
+}
+
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+   document.exitFullscreen();
+} else if (document.webkitExitFullscreen) {
+   document.webkitExitFullscreen();
+} else if (document.msExitFullscreen) {
+   document.msExitFullscreen();
+ }
+}
